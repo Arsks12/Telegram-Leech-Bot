fff871922b22cc43aba3ab78f1ee2cafcd5c8e45
Arsks12 <101000780+Arsks12@users.noreply.github.com>
2024-08-26 22:38:31 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -2,11 +2,16 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-	TG_BOT_TOKEN= "1268fill---your----data"
-	APP_ID = 1373938
-	API_HASH = ""
-	OWNER_ID = "" #ID of bot owner
+	TG_BOT_TOKEN= "7206702826:AAFiQoFSuXe4o1MVWkn77w0yEe_SmMIAVnY"
+	APP_ID = 7068671
+	API_HASH = "76c7e8041eaf80e335127a33b5618c96"
+	OWNER_ID = "873713243" #ID of bot owner
 	AUTH_CHANNEL = [-1002202897360]
 	DESTINATION_FOLDER = "HelloMan" #Name of your folder read readme
-	RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
+	RCLONE_CONFIG = """[Arima 27]
+type = drive
+scope = drive
+token = {"access_token":"ya29.a0AXooCguZ2WncJiQB8MIdkDpkzO4KXUB4ohGeZhNt306nzI3H-hRay5b6ToWUV3Fn_C6wq3T3_2uRFhV0znZV5xXQde4gmCOPoac3OX1dIc_WeYCFMtCfCFgpnRW8Jxp8dxbEzd35k5Y2E6QSFloVbNFJU2982mZLquywzAaCgYKAeYSARMSFQHGX2MinV0w9FT0WlaUYgFa7cMpIw0173","token_type":"Bearer","refresh_token":"1//0gBhhniNyOSHqCgYIARAAGBASNwF-L9Irs0HYbk9TtdwOTcv4zGeBQtPJ7oDVdiz8JT0aEv3yY57-9MXNYRdtf6Vyt7me4eVRQW4","expiry":"2024-07-28T16:56:52.6490397+05:30"}
+team_drive = 0ANfpqOOWza3xUk9PVA
+root_folder_id = """
 	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
